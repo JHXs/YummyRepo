6ac9117e855b134a1d60cf2a6d6a4027cdb7e148
hansel <58466533+JHXs@users.noreply.github.com>
2026-04-06 12:23:56 +0800

    更新 .gitignore 文件以忽略 packages 目录，并修改 fetch.sh 脚本以过滤下载链接

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -7,14 +7,14 @@ cd packages
 # 你要抓的 GitHub 项目列表
 REPOS=(
   "chen08209/FlClash"
-#   "xishang0128/sparkle"
+  "xishang0128/sparkle"
 )
 
 for repo in "${REPOS[@]}"; do
   echo "Fetching $repo ..."
 
   urls=$(curl -s https://api.github.com/repos/$repo/releases/latest \
-    | jq -r '.assets[] | select(.name | endswith(".rpm")) | .browser_download_url')
+    | jq -r '.assets[] | select(.name | endswith(".rpm") and (contains("x86_64") or contains("amd64"))) | .browser_download_url')
 
   for url in $urls; do
     echo "Downloading $url"
